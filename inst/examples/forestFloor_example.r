287c8e20151517b6ee10c0325b4e5b4dd270bf27
sorhawell <sorhawell@gmail.com>
2016-04-11 13:00:11 +0200

    move from source to examples

--- inst/examples/forestFloor_example.r
+++ inst/examples/forestFloor_example.r
@@ -31,15 +31,15 @@ Col = fcol(ff,3,orderByImportance=FALSE)
 plot(ff,col=Col) 
 
 #in 3D the interaction between X3 and X reveals itself completely
-show3d_new(ff,3:4,col=Col,plot.rgl=list(size=5)) 
+show3d(ff,3:4,col=Col,plot.rgl=list(size=5)) 
 
 #although no interaction, a joined additive effect of X1 and X2
 #colour by FC-component FC1 and FC2 summed
 Col = fcol(ff,1:2,orderByImportance=FALSE,X.m=FALSE,RGB=TRUE)
 plot(ff,col=Col) 
-show3d_new(ff,1:2,col=Col,plot.rgl=list(size=5)) 
+show3d(ff,1:2,col=Col,plot.rgl=list(size=5)) 
 
 #...or two-way gradient is formed from FC-component X1 and X2.
 Col = fcol(ff,1:2,orderByImportance=FALSE,X.matrix=TRUE,alpha=0.8) 
 plot(ff,col=Col) 
-show3d_new(ff,1:2,col=Col,plot.rgl=list(size=5))
+show3d(ff,1:2,col=Col,plot.rgl=list(size=5))
